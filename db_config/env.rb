7ecab7cc91279fef1a3074bd16852aad096c87d4
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-10-18 22:52:25 +0100

    Changed LOGGER level.

--- db_config/env.rb
+++ db_config/env.rb
@@ -6,3 +6,4 @@ env = ENV['RACK_ENV'] || 'development'
 puts "Connected to the #{env} database."
 
 Mongoid.load!(File.expand_path("./db_config/mongoid.yml"), env)
+Mongo::Logger.logger.level = ::Logger::FATAL
