0f410b67e9deccaf5c6c0c18cf6efe8035b4bc22
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-10-20 00:38:16 +0100

    Implemented a story positivity score.

--- lib/story.rb
+++ lib/story.rb
@@ -1,9 +1,28 @@
 class Story
   attr_reader :id, :headline, :body
 
+  POSITIVE_WORDS = ['positive', 'success', 'grow', 'gains', 'happy', 'healthy'       ]
+  NEGATIVE_WORDS = ['dissapointing', 'concerns', 'decline', 'drag', 'slump', 'feared']
+
   def initialize(args = {})
     @id       = args[:id]
     @headline = args[:headline]
     @body     = args[:body]
+  end
+
+  def positivity_score
+    words.reduce(0) { |memo, word| memo + score_for_word(word) }
+  end
+
+  private
+
+  def words
+    body.split(/\W+/).map { |word| word.downcase }
+  end
+
+  def score_for_word(word)
+    return 1  if POSITIVE_WORDS.include?(word)
+    return -1 if NEGATIVE_WORDS.include?(word)
+    return 0
   end
 end
