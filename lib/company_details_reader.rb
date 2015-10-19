1f44429d60f19d2f7e99a4effc3b4167c042f8d0
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-10-19 01:54:43 +0100

    CompanyDetailsReader can retrieve data now.

--- lib/company_details_reader.rb
+++ lib/company_details_reader.rb
@@ -1,7 +1,14 @@
+require 'httparty'
+
 class CompanyDetailsReader
+  BASE_URI = 'http://mm-recruitment-stock-price-api.herokuapp.com/company/'
   attr_reader :ticker_code
 
   def initialize(ticker_code)
     @ticker_code = ticker_code
+  end
+
+  def retrieve_data
+    HTTParty.get(BASE_URI + ticker_code)
   end
 end
--- spec/company_details_reader_spec.rb
+++ spec/company_details_reader_spec.rb
@@ -1,8 +1,15 @@
 require 'company_details_reader'
 
 describe CompanyDetailsReader do
+  let(:reader) { CompanyDetailsReader.new('AAPL') }
+
   it 'is initialized with a ticker code' do
-    reader = CompanyDetailsReader.new('AAPL')
     expect(reader.ticker_code).to eq 'AAPL'
+  end
+
+  it 'can make a GET request to an external web service' do
+    expect(HTTParty).to receive(:get).with(
+      "#{CompanyDetailsReader::BASE_URI}" + reader.ticker_code)
+    reader.retrieve_data
   end
 end
