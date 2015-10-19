378b9c79f4a0b2bfbdad600ce8ffdaa0718f34c7
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-10-19 21:25:37 +0100

    Implemented the basic functionality for a story.

--- lib/story.rb
+++ lib/story.rb
@@ -1,2 +1,9 @@
 class Story
+  attr_reader :id, :headline, :body
+
+  def initialize(args = {})
+    @id       = args[:id]
+    @headline = args[:headline]
+    @body     = args[:body]
+  end
 end
--- spec/libraries/story_spec.rb
+++ spec/libraries/story_spec.rb
@@ -1,5 +1,17 @@
 require 'story'
 
 describe Story do
-  
+  let(:story) { Story.new(id: 12, headline: 'Some headline', body: 'Some text body') }
+
+  it 'has an id' do
+    expect(story.id).to eq 12
+  end
+
+  it 'has a headline' do
+    expect(story.headline).to eq 'Some headline'
+  end
+
+  it 'has a body' do
+    expect(story.body).to eq 'Some text body'
+  end
 end
