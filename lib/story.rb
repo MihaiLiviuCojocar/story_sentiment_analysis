946318d33f87c14ef237695ae68f8d7b4490bcc4
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-10-20 22:12:09 +0100

    Refactored Story class: moved the functionality for calculating the score for a word into the String class.

--- lib/story.rb
+++ lib/story.rb
@@ -1,9 +1,6 @@
 class Story
   attr_reader :id, :headline, :body
 
-  POSITIVE_WORDS = ['positive', 'success', 'grow', 'gains', 'happy', 'healthy'       ]
-  NEGATIVE_WORDS = ['dissapointing', 'concerns', 'decline', 'drag', 'slump', 'feared']
-  
   POSITIVE_RANGE = Range.new(2,Float::INFINITY)
   NEGATIVE_RANGE = Range.new(-Float::INFINITY,-1)
   NEUTRAL_RANGE  = Range.new(0,1)
@@ -27,7 +24,7 @@ class Story
   end
 
   def positivity_score
-    words.reduce(0) { |memo, word| memo + score_for_word(word) }
+    normalised_words.reduce(0) { |memo, word| memo + word.sentiment_score }
   end
 
   def sentiment_analysis
@@ -40,13 +37,7 @@ class Story
 
   private
 
-  def words
-    body.split(/\W+/).map { |word| word.downcase }
-  end
-
-  def score_for_word(word)
-    return 1  if POSITIVE_WORDS.include?(word)
-    return -1 if NEGATIVE_WORDS.include?(word)
-    return 0
+  def normalised_words
+    body.split(/\W+/).map(&:downcase)
   end
 end
