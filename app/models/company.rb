cde74a06033a47806eb4e08694e0426854aaa6b7
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-10-21 23:24:36 +0100

    Reorganised the file structure.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,5 @@
 require 'mongoid'
+require 'httparty'
 
 class Company
   include Mongoid::Document
@@ -9,6 +10,8 @@ class Company
   store_in collection: 'company'
 
   NOT_AVAILABLE_YET = 'N/A'
+
+  BASE_URI = 'http://mm-recruitment-stock-price-api.herokuapp.com/company/'
 
   def latest_price
     @latest_price || NOT_AVAILABLE_YET
@@ -44,17 +47,23 @@ class Company
   private
 
   def set_primary_details_from_parsed
-    company = JSON.parse(CompanyDetailsReader.new(tickerCode).retrieve_data)
-    @latest_price   = company["latestPrice"]
-    @price_units    = company["priceUnits"]
-    @as_of          = company["asOf"]
-    @story_feed_uri = company["storyFeedUrl"]
+    response = HTTParty.get(BASE_URI + tickerCode)
+    if response.code == 200
+      parsed = JSON.parse(response.body)
+      @latest_price   = parsed["latestPrice"]
+      @price_units    = parsed["priceUnits"]
+      @as_of          = parsed["asOf"]
+      @story_feed_uri = parsed["storyFeedUrl"]
+    end
   end
 
   def set_stories_from_parsed
-    stories = JSON.parse(CompanyNewsFeedRader.new(story_feed_uri).retrieve_data)
-    stories.each do |story|
-      add_story(Story.new(id: story["id"], headline: story["headline"], body: story["body"]))
+    response = HTTParty.get(story_feed_uri)
+    if response.code == 200
+      stories = JSON.parse(response.body)
+      stories.each do |story|
+        add_story(Story.new(id: story["id"], headline: story["headline"], body: story["body"]))
+      end
     end
   end
 
